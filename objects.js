9bc02ef44afeb3ae4738ac946a1e83c7d07a1b3c
jake.c.rummel@gmail.com <jake.c.rummel@gmail.com>
2018-08-17 02:51:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,7 +11,7 @@ function updatePlaylist (playlist, artistName, songTitle) {
 
 
 function removeFromPlaylist (playlist, artistName) {
-  if ( delete playlist.artistName )
+  delete playlist.artistName 
   return playlist
 }
 
