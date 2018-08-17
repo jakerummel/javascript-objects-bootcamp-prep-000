933216878e08bff2daee12eb335416495ab45926
jake.c.rummel@gmail.com <jake.c.rummel@gmail.com>
2018-08-17 02:44:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { 
-  kanye: "Jesus Walks", 
-  journey: "Don't Stop Believing"
+  'My Bloody Valentine': 'Sometimes', 
+  'Phil Ochs': "Here's to the State of Mississippi"
 }
 
 function updatePlaylist (playlist, artistName, songTitle) {
@@ -11,6 +11,7 @@ function updatePlaylist (playlist, artistName, songTitle) {
 
 function removeFromPlaylist (playlist, artistName) {
   delete playlist.artistName
+  return playlist
 }
 
 //Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
